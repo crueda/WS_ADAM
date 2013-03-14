e2c1a599bdc9dfba5bb8912617d3b41ff060c360
Carlos Rueda Morales <crueda@gmail.com>
2013-03-14 18:09:35 +0100

    new version

--- WS_adam/src/com/deimos/adam/ws/RouteWS.java
+++ WS_adam/src/com/deimos/adam/ws/RouteWS.java
@@ -28,48 +28,30 @@ public class RouteWS {
 	
 	Connection conn;
               
-    String dbname = "pgrouting";
+    String dbname = "adam";
     String dbuser = "user";
     String dbpass = "user";
-	String dbhost = "172.26.0.240";
+	String dbhost = "172.26.0.2";
 	String dbport = "5432";
 	               
-    // String dbtable = "jdbc_test";
-	               
-	/*    String dropSQL = "drop table " + dbtable;
-	                String createSQL = "create table " + dbtable + " (geom geometry, id int4)";
-	                String insertPointSQL = "insert into " + dbtable + " values ('POINT (10 10 10)',1)";
-	                String insertPolygonSQL = "insert into " + dbtable + " values ('POLYGON ((0 0 0,0 10 0,10 10 0,10 0 0,0 0 0))',2)";
-	*/ 
+
 	try {
 	               
 		System.out.println("Creating JDBC connection...");
 	    Class.forName("org.postgresql.Driver");
 	    String url = "jdbc:postgresql://" + dbhost + ":" + dbport + "/" + dbname;
 	    conn = DriverManager.getConnection(url, dbuser, dbpass);
-	    //  System.out.println("Adding geometric type entries...");
-	    //  ((org.postgresql.Connection)conn).addDataType("geometry","org.postgis.PGgeometry");
-	    //  ((org.postgresql.Connection)conn).addDataType("box3d","org.postgis.PGbox3d");
 	    Statement s = conn.createStatement();
-	    /*System.out.println("Creating table with geometric types...");
-	                        s.execute(dropSQL);
-	                        s.execute(createSQL);
-	                        System.out.println("Inserting point...");
-	                        s.execute(insertPointSQL);
-	                        System.out.println("Inserting polygon...");
-	                        s.execute(insertPolygonSQL);
-	                        System.out.println("Done.");
-	                        */
-	                        //s = conn.createStatement();
 	                        
 	    System.out.println("Querying ...");
-	    ResultSet r = s.executeQuery("SELECT * FROM shortest_path('SELECT gid as id,source::integer,target::integer,length::double precision as cost FROM ways',100, 600, false, false);");
+	    
+	    String query = "SELECT * FROM shortest_path('SELECT gid as id,source::integer,target::integer,length::double precision as cost FROM ways'," + initVertex + ", " +endVertex+ ", false, false);";
+	    ResultSet r = s.executeQuery(query);
 	    while( r.next() )
 	    {
 	    	Object obj =  r.getObject(1);
 	        int id = r.getInt(2);
 	        System.out.println("Row " + id + ":");
-	        //System.out.println(obj.toString());
 	                                
 	        vertex = new VertexDto();
 	        vertex.setId(id);
